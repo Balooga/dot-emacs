5e559b04a83f18cf89b97a722c2f7002ff97cb07
Luke Crook <luke@balooga.com>
2014-10-27 21:40:12 -0700

    - Updates.

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -1,25 +1,25 @@
 ;;; http://orgmode.org/worg/org-contrib/babel/intro.html
 ;;; init.el --- Where all the magic begins
 ;;
-(defvar dotfiles-dir (file-name-directory (or (buffer-file-name)
-					      load-file-name)))
 
+;; Set org-mode-path-name to the location of latest version of org-mode
 (defvar org-mode-path-name "~/site/org-mode")
 
-;; Set the directory for the latest version of org-mode
+;; Load the latest version of org-mode
 (defvar org-dir (expand-file-name org-mode-path-name ""))
 (defvar org/lisp-dir (expand-file-name "lisp" org-dir))
 (defvar org/contrib-dir (expand-file-name "contrib/lisp" org-dir))
 (add-to-list 'load-path org/lisp-dir)
-(add-to-list 'load-path org/contrib-dir t)
+(add-to-list 'load-path org/contrib-dir)
 
 (require 'org)
 (require 'org-install)
 (require 'ob-tangle)
 (require 'org-id)
 
-;; Load up all literate org-mode files in this directory
-;; In other words, load emacs.org
+;; Load emacs.org from the .emacs.d directory
+(defvar dotfiles-dir (file-name-directory (or (buffer-file-name)
+					      load-file-name)))
 (mapc #'org-babel-load-file 
       (list (expand-file-name "emacs.org" dotfiles-dir)))
 
